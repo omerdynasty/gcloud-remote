1ad7beaf57dd47fdc001a12035aa7d5a26442d6f
Ömer <74985599+omerdynasty@users.noreply.github.com>
2023-08-28 08:07:37 +0300

    Update gcremote.sh

--- gcremote.sh
+++ gcremote.sh
@@ -8,7 +8,7 @@ sudo apt install ./chrome-remote-desktop_current_amd64.deb
 sudo DEBIAN_FRONTEND=noninteractive \
     apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver;sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo systemctl disable lightdm.service
-sudo apt install chromium
+sudo apt install firefox-esr
 clear
 echo ========================
 echo Kurulum bitti
